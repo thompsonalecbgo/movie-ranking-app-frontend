8e0d1bdfbdb45d07d9b72aa3241c6c9d9ed1758d
Thompson Go <thompsongo2020@gmail.com>
2020-11-18 18:32:54 +0800

    Connection to backend setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axiosInstance from './axiosInstance';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+    }
+  }
+  componentDidMount() {
+    axiosInstance.get('/')
+      .then(response => {
+        console.log(response)
+        this.setState({message: response.data.message})
+      })
+      .catch(err => {
+        throw err;
+      })
+  }
+  render() {
+    return (
+      <div>
+        {this.state.message}
+      </div>
+    )
+  }
 }
 
 export default App;
