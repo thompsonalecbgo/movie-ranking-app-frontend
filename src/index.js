1c80379851a634e6faca04761edbe16a4d64190b
Thompson Go <thompsongo2020@gmail.com>
2020-11-28 08:01:24 +0800

    removed react react redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
@@ -10,9 +9,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
         <App />
-      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
